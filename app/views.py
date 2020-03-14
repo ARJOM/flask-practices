3efa707d66dc15f2f6e9359540c280328561c05a
Ricart <ricarteusuo11@gmail.com>
2020-03-14 01:43:22 -0300

    renders dynamic content with flask

--- app/views.py
+++ app/views.py
@@ -1,6 +1,10 @@
+from flask import render_template
+
 from app import app
 
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    title = "Olá Mundo"
+    paragraph = "Este é o primeiro teste sem preconceito com flask"
+    return render_template('base.html', title=title, paragraph=paragraph)
