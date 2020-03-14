5e4996c810fe88ce30b3dc6990609cb29608167a
Ricart <ricarteusuo11@gmail.com>
2020-03-14 14:43:08 -0300

    add database connection and list registered users

--- app/views.py
+++ app/views.py
@@ -1,10 +1,37 @@
-from flask import render_template
+from flask import render_template, g
+
+import psycopg2.extras
 
 from app import app
 
+
+# ~~~~
+@app.before_request
+def before_request():
+    g.db = psycopg2.connect('dbname=flask-practice user=flask password=flask host=127.0.0.1')
+
+
+@app.teardown_request
+def teardown_request(exception):
+    g.db.close()
+
+
+# ~~~~
 
 @app.route('/')
 def index():
     title = "Olá Mundo"
     paragraph = "Este é o primeiro teste sem preconceito com flask"
     return render_template('home.html', title=title, paragraph=paragraph)
+
+
+@app.route('/users', methods=['GET'])
+def users():
+    cur = g.db.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    cur.execute("SELECT * FROM users")
+
+    data = cur.fetchall()
+    cur.close()
+
+    return render_template('users.html', users=data)
---
+++ db/practice.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users(
+	id serial,
+	name varchar(80),
+	constraint user_pk primary key(id)
+)
